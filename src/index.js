ed303e5a90e03fe03aa91e9477e3c5981702a6aa
David Godfrey <david@palatialxr.com>
2023-06-05 17:22:53 +0000

    Remove uiFeatures display

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import 'bootstrap/dist/js/bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/css/bootstrap-grid.min.css'
-import 'bootstrap/dist/css/bootstrap-reboot.min.css'
-import 'bootstrap/dist/css/bootstrap-utilities.min.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { delegate, config, playerElement } from './DOMDelegate';
