ea751bedefad309a25cd41deedaa0ba141bdcf57
David Godfrey <david@palatialxr.com>
2023-06-16 14:41:41 +0000

    Noop

--- src/App.js
+++ src/App.js
@@ -174,16 +174,16 @@ function App() {
       } else {
         mobileUser = false;
       }
-  
+
       const deviceTypeMessage = JSON.stringify({ mobileUser: mobileUser });
       console.log(deviceTypeMessage);  // Print out the message for testing
 
-      
+
       // I'm not sure where you want to send this message, so I'm leaving this part for you to implement.
     };
-  
+
     detectDeviceType();
-    
+
   }, []);
 
 
